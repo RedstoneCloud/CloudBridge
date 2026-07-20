3502323b519c5d42a3bd559952272ed9ceb1c3e9
Adrian <adrian@wanmine.net>
2026-07-20 02:49:30 +0200

    Migrate to PNX 3.0

--- src/main/java/de/redstonecloud/bridge/platform/waterdogpe/commands/ServerCommand.java
+++ src/main/java/de/redstonecloud/bridge/platform/waterdogpe/commands/ServerCommand.java
@@ -59,12 +59,12 @@ public class ServerCommand extends Command {
         CommandParamData server = new CommandParamData();
         server.setName("server");
         server.setOptional(false);
-        server.setType(CommandParam.TEXT);
+        server.setType(CommandParam.ID);
 
         CommandParamData player = new CommandParamData();
         player.setName("player");
         player.setOptional(true);
-        player.setType(CommandParam.TARGET);
+        player.setType(CommandParam.NAME_ARG);
         return new CommandOverloadData[]{new CommandOverloadData(false, new CommandParamData[]{server, player})};
     }
 }
