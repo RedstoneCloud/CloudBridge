3180e0a65452f38708e76d304331cbfa38af86bf
Adrian <adrian@wanmine.net>
2026-07-20 02:51:04 +0200

    Remove unused imports, make server final

--- src/main/java/de/redstonecloud/bridge/platform/nukkit/NukkitExecutor.java
+++ src/main/java/de/redstonecloud/bridge/platform/nukkit/NukkitExecutor.java
@@ -9,10 +9,9 @@ import de.redstonecloud.bridge.cloudinterface.components.BridgeExecutor;
 import de.redstonecloud.bridge.cloudinterface.components.BridgeServer;
 
 import java.util.Objects;
-import java.util.UUID;
 
 public class NukkitExecutor implements BridgeExecutor {
-    private static Server server = Server.getInstance();
+    private static final Server server = Server.getInstance();
 
     public Player getPlayerByCloudPlayer(ICloudPlayer player) {
         return server.getPlayer(player.getUUID()).get();
